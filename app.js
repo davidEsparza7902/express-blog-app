ffdfb42b4e516758c3be792422209b2147b41a82
davidEsparza7902 <desparzas7902@gmail.com>
2023-10-02 06:32:04 -0500

    add blogController

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const config = require('./utils/config')
 const logger = require('./utils/logger')
 const express = require('express')
-const app = express()
+
 const cors = require('cors')
 const mongoose = require('mongoose')
-const Blog = require('./models/blog')
+const BlogRouter = require ('./controllers/blogs')
 
 const mongoUrl = config.MONGODB_URI
+
 logger.info('connecting')
 mongoose.connect(mongoUrl).then(()=>{
     logger.info('connected to MongoDB')
@@ -14,27 +15,10 @@ mongoose.connect(mongoUrl).then(()=>{
     logger.info('could not connect', e)
 })
 
+const app = express()
+
 app.use(cors())
 app.use(express.json())
+app.use('/api/blogs',BlogRouter)
 
-app.get('/', (req, res) => {
-    res.send('holaaaaaaa')
-})
-app.get('/api/blogs', (request, response) => {
-    Blog
-        .find({})
-        .then(blogs => {
-            response.json(blogs)
-        })
-})
-
-app.post('/api/blogs', (request, response) => {
-    const blog = new Blog(request.body)
-
-    blog
-        .save()
-        .then(result => {
-            response.status(201).json(result)
-        })
-})
 module.exports = app
