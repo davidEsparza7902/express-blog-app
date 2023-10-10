3ce05404e9d0a6364f0b71fdad207f7a523c92f9
davidEsparza7902 <desparzas7902@gmail.com>
2023-10-10 11:13:42 -0500

    add jwt

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,6 +1,15 @@
 const Blog = require('../models/blog')
 const User = require('../models/user')
 const blogRouter = require('express').Router()
+const jwt = require('jsonwebtoken')
+
+const getTokenFrom = request => {
+    const authorization = request.get('authorization')
+    if (authorization && authorization.startsWith('bearer ')) {
+        return authorization.replace('bearer ', '')
+    }
+    return null
+}
 
 blogRouter.get('/', async (request, response) => {
     const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
@@ -9,8 +18,13 @@ blogRouter.get('/', async (request, response) => {
 
 blogRouter.post('/', async (request, response) => {
     const body = request.body
-    // Temporary solution for the user field
-    const user = (await User.findOne({}))
+    const token = getTokenFrom(request)
+    const decodedToken = jwt.verify(token, process.env.SECRET)
+    if (!token || !decodedToken.id){
+        return response.status(401).json({ error: 'token missing or invalid' })
+    }
+
+    const user = await User.findById(decodedToken.id)
     if (!body['likes'])
         body['likes'] = 0
 
